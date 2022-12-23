b9365e61d4415028373de44c8a02f9c2de839d13
Mirumo <mysteryperson520@gmail.com>
2022-12-23 10:23:26 +0800

    feat: set global container style

--- src/App.vue
+++ src/App.vue
@@ -2,3 +2,14 @@
   <router-view></router-view>
 </template>
 <script setup></script>
+<style>
+body,
+#app {
+  width: 100vw;
+  height: 100vh;
+  background: transparent;
+  border: 0;
+  margin: 0;
+  padding: 0;
+}
+</style>
