f73d247bdee776bf92f0860a1c7bab6378b7e564
Mirumo <mysteryperson520@gmail.com>
2022-12-23 12:01:51 +0800

    feat: aside component for products list

--- src/Components/ProductsAside.vue
+++ src/Components/ProductsAside.vue
@@ -1,3 +1,46 @@
 <template>
-  <aside>aside</aside>
+  <aside>
+    <span>產品清單</span>
+    <a v-for="p in list" :href="`#${p}`">{{ p }}</a>
+  </aside>
 </template>
+
+<script>
+import list from '../templates/productsList.json';
+
+export default {
+  data() {
+    return { list };
+  }
+};
+</script>
+
+<style scoped>
+aside {
+  background-color: #00e073;
+  display: flex;
+  flex-direction: column;
+  width: max-content;
+  height: max-content;
+  row-gap: 20px;
+  border-radius: 10px;
+  padding: 15px 20px;
+  justify-self: center;
+  margin: 25px auto;
+  position: sticky;
+  top: 25px;
+}
+
+span {
+  font-size: 1rem;
+  color: #9d008d;
+  font-weight: bold;
+  opacity: 0.75;
+}
+
+a {
+  color: inherit;
+  text-decoration: none;
+  font-size: 1.2rem;
+}
+</style>
